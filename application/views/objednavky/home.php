810cf4bce3c7cf6ee315620cdc528e26cccd1a60
galis30 <3.galis.galis.3gmail.com>
2018-05-09 17:43:11 +0200

    1.0.4 Pridaný kalendár

--- application/views/objednavky/home.php
+++ application/views/objednavky/home.php
@@ -1,5 +1,28 @@
 <div class="container">
 
-    ...
+    <div class="jumbotron">
+        <h1>Bootstrap Tutorial</h1>
+        <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive,
+            mobile-first projects on the web.</p>
+    </div>
+    <div class="container">
+        <p>This is some text.</p>
+        <p>This is another text.</p>
+    </div>
+
+    <html>
+    <head>
+        <link href="calendar.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body>
+    <?php
+    include 'calendar.php';
+
+    $calendar = new Calendar();
+
+    echo $calendar->show();
+    ?>
+    </body>
+    </html>
 
 </div><!-- /.container -->
