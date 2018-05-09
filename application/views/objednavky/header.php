768a8b76ad68e76112cfeaf89ac9b16408c08579
galis30 <3.galis.galis.3gmail.com>
2018-05-09 17:25:39 +0200

    1.0.2 Vytvorenie Bootsrap sablona pre strukturu stranky

---
+++ application/views/objednavky/header.php
@@ -0,0 +1,2 @@
+<!-- Bootstrap core CSS --> <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap theme --> <link href="<?php echo base_url();?>assets/css/bootstraptheme.min.css" rel="stylesheet">
